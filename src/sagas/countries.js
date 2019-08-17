49a8fe6055f6acdcc60623043fe75d0e2bcf93cc
efe <jimmyfallon717@gmail.com>
2019-08-17 18:05:08 +0300

    sagas created / store.js modified

---
+++ src/sagas/countries.js
@@ -0,0 +1,15 @@
+import {call, takeLatest, put} from 'redux-saga/effects';
+import {CountriesActionTypes as ActionTypes} from '../types/ActionTypes';
+
+function* fetchCountriesSaga(action) {
+    try {
+        //TODO: call the api here
+    } catch (e){
+        //TODO: catch the errors here
+    }
+
+}
+
+export default [
+    takeLatest(ActionTypes.fetch_countries, fetchCountriesSaga),
+];
